b072080cc5629fcefe35764325f1e4187cd303d1
sachin8516 <sachin8516@gmail.com>
2019-02-09 18:48:39 +0530

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client?reload=true");
+require("bootstrap/dist/css/bootstrap-grid.min.css");
+require("../stylus/main.styl");
+require("jquery");
+require("popper.js");
+require("bootstrap/dist/js/bootstrap.min");
+require("../index.html");
+
